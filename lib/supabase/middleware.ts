4842093c0070d8b568a8329d2a23563e6033c307
Thales <thales.sato@multipedidos.com.br>
2025-11-26 17:27:04 -0300

    🐛 Fix: remove auth to use script

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -33,7 +33,8 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/magic-banner.js')
   ) {
     const url = request.nextUrl.clone()
     url.pathname = '/auth/login'
